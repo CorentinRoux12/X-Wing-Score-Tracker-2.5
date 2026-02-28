63913846c059d3323a6bd8df7bf3df6105745864
Roux Corentin <corentin.roux@etu.univ-orleans.fr>
2026-02-28 15:04:02 +0100

    :sparkles: Ajout de fonctionnalité et correction de view

--- app/src/main/java/fr/corentin/roux/x_wing_score_tracker/ui/activities/SettingsActivity.java
+++ app/src/main/java/fr/corentin/roux/x_wing_score_tracker/ui/activities/SettingsActivity.java
@@ -224,6 +224,7 @@ public class SettingsActivity extends AbstractActivity
 
     private void reloadDarkMode()
     {
+//        Toast.makeText(this, "Don't click to fast my young apprentice.", Toast.LENGTH_SHORT).show();
         AppCompatDelegate.setDefaultNightMode(Boolean.TRUE.equals(this.enabledDarkMode) ?
                 AppCompatDelegate.MODE_NIGHT_YES :
                 AppCompatDelegate.MODE_NIGHT_NO);
@@ -231,6 +232,7 @@ public class SettingsActivity extends AbstractActivity
 
     private void startSettingsActivity()
     {
+        //Toast.makeText(this, "Recreate Incomming", Toast.LENGTH_SHORT).show();
         this.recreate();
     }
 
@@ -268,4 +270,17 @@ public class SettingsActivity extends AbstractActivity
         this.setting.setPathRingTone(this.pathRingTone);
         this.service.save(this, this.setting);
     }
+
+
+//    @Override
+//    public void onMusicPick(@NonNull Uri uri, @NonNull String s)
+//    {
+//        this.pathRingTone = uri.toString();
+//    }
+//
+//    @Override
+//    public void onPickCanceled()
+//    {
+//        //Nothing to declare
+//    }
 }
